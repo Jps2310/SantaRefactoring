f3009d42e2564942e93e0f979ad89e9630367d8c
Jps2310 <janphillip.fps@gmail.com>
2021-03-23 10:17:54 +0100

    delete unused methods

--- Assets/MyGame/Scripts/TextureScroll.cs
+++ Assets/MyGame/Scripts/TextureScroll.cs
@@ -16,18 +16,6 @@ public class TextureScroll : MonoBehaviour
 
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
         if (scroll)
