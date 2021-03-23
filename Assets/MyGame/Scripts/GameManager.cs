7b570d047fe6d8204094b3c57dfdcfdcaabf290c
Jps2310 <janphillip.fps@gmail.com>
2021-03-23 10:06:36 +0100

    delete unused methods

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -18,17 +18,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("in Start");   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("in Update");
-    }
+   
 
     public void GameOver()
     {
