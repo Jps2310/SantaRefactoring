102631a90b3249dbb1268ad47ac2166c1d4b8f3e
Jps2310 <janphillip.fps@gmail.com>
2021-03-23 10:14:24 +0100

    delete unused namespaces and methods

--- Assets/MyGame/Scripts/ObstacleSpawner.cs
+++ Assets/MyGame/Scripts/ObstacleSpawner.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
@@ -27,11 +26,7 @@ public class ObstacleSpawner : MonoBehaviour
         StartCoroutine("Spawn");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
     IEnumerator Spawn()
     {
